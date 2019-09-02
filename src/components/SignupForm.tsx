efd14332095265d0baaec0f7caee09ebea0a1d13
topsoftwareguru <janepress940214@outlook.com>
2019-09-02 09:58:14 +0300

    styling for show/hide password && checkmark

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -2,6 +2,8 @@ import React, { Component, FormEvent } from 'react';
 import { Link } from "react-router-dom";
 
 import Description from "./Description";
+import BallotImage from "../assets/images/ballot-check.png";
+import BallotImageUncheck from "../assets/images/ballot-uncheck.png";
 
 const emailRegex = RegExp(
   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
@@ -14,6 +16,110 @@ const passwordRegexp = RegExp(
 const errorsvg = `
 <?xml version="1.0" ?><!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.0//EN'  'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'><svg height="8" style="overflow:visible;enable-background:new 0 0 16 16" viewBox="0 0 16 16" width="16" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><g id="Error_1_"><g id="Error"><circle cx="16" cy="16" id="BG" r="16" style="fill:#D72828;"/><path d="M14.5,25h3v-3h-3V25z M14.5,6v13h3V6H14.5z" id="Exclamatory_x5F_Sign" style="fill:#E6E6E6;"/></g></g></g></svg>`
 
+const showsvg = `
+<?xml version="1.0" encoding="iso-8859-1"?>
+<!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 59.2 59.2" style="enable-background:new 0 0 59.2 59.2;" xml:space="preserve">
+<g>
+	<path d="M51.062,21.561c-11.889-11.889-31.232-11.889-43.121,0L0,29.501l8.138,8.138c5.944,5.944,13.752,8.917,21.561,8.917
+		s15.616-2.972,21.561-8.917l7.941-7.941L51.062,21.561z M49.845,36.225c-11.109,11.108-29.184,11.108-40.293,0l-6.724-6.724
+		l6.527-6.527c11.109-11.108,29.184-11.108,40.293,0l6.724,6.724L49.845,36.225z" style="fill:#fff"/>
+	<path d="M28.572,21.57c-3.86,0-7,3.14-7,7c0,0.552,0.448,1,1,1s1-0.448,1-1c0-2.757,2.243-5,5-5c0.552,0,1-0.448,1-1
+		S29.125,21.57,28.572,21.57z" style="fill:#fff"/>
+	<path d="M29.572,16.57c-7.168,0-13,5.832-13,13s5.832,13,13,13s13-5.832,13-13S36.741,16.57,29.572,16.57z M29.572,40.57
+		c-6.065,0-11-4.935-11-11s4.935-11,11-11s11,4.935,11,11S35.638,40.57,29.572,40.57z" style="fill:#fff"/>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>
+`
+
+const hidesvg = `
+  <?xml version="1.0" encoding="iso-8859-1"?>
+  <!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+  <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    viewBox="0 0 59.049 59.049" style="enable-background:new 0 0 59.049 59.049;" xml:space="preserve">
+  <g>
+	<path d="M11.285,41.39c0.184,0.146,0.404,0.218,0.623,0.218c0.294,0,0.585-0.129,0.783-0.377c0.344-0.432,0.273-1.061-0.159-1.405
+		c-0.801-0.638-1.577-1.331-2.305-2.06l-7.398-7.398l7.629-7.629c7.334-7.333,18.003-9.836,27.839-6.534
+		c0.523,0.173,1.09-0.107,1.267-0.63c0.175-0.523-0.106-1.091-0.63-1.267c-10.562-3.545-22.016-0.857-29.89,7.016L0,30.368
+		l8.812,8.812C9.593,39.962,10.426,40.705,11.285,41.39z" style="fill:#fff"/>
+	<path d="M50.237,21.325c-1.348-1.348-2.826-2.564-4.394-3.616c-0.458-0.307-1.081-0.185-1.388,0.273
+		c-0.308,0.458-0.185,1.08,0.273,1.388c1.46,0.979,2.838,2.113,4.094,3.369l7.398,7.398l-7.629,7.629
+		c-7.385,7.385-18.513,9.882-28.352,6.356c-0.52-0.187-1.093,0.084-1.279,0.604c-0.186,0.52,0.084,1.092,0.604,1.279
+		c3.182,1.14,6.49,1.693,9.776,1.693c7.621,0,15.124-2.977,20.665-8.518l9.043-9.043L50.237,21.325z" style="fill:#fff"/>
+	<path d="M30.539,41.774c-2.153,0-4.251-0.598-6.07-1.73c-0.467-0.29-1.084-0.148-1.377,0.321c-0.292,0.469-0.148,1.085,0.321,1.377
+		c2.135,1.33,4.6,2.032,7.126,2.032c7.444,0,13.5-6.056,13.5-13.5c0-2.685-0.787-5.279-2.275-7.502
+		c-0.308-0.459-0.93-0.582-1.387-0.275c-0.459,0.308-0.582,0.929-0.275,1.387c1.267,1.893,1.937,4.102,1.937,6.39
+		C42.039,36.616,36.88,41.774,30.539,41.774z" style="fill:#fff"/>
+	<path d="M30.539,18.774c2.065,0,4.089,0.553,5.855,1.6c0.474,0.281,1.088,0.125,1.37-0.351c0.281-0.475,0.125-1.088-0.351-1.37
+		c-2.074-1.229-4.451-1.879-6.875-1.879c-7.444,0-13.5,6.056-13.5,13.5c0,2.084,0.462,4.083,1.374,5.941
+		c0.174,0.354,0.529,0.56,0.899,0.56c0.147,0,0.298-0.033,0.439-0.102c0.496-0.244,0.701-0.843,0.458-1.338
+		c-0.776-1.582-1.17-3.284-1.17-5.06C19.039,23.933,24.198,18.774,30.539,18.774z" style="fill:#fff"/>
+	<path d="M54.621,5.567c-0.391-0.391-1.023-0.391-1.414,0l-46.5,46.5c-0.391,0.391-0.391,1.023,0,1.414
+		c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l46.5-46.5C55.012,6.591,55.012,5.958,54.621,5.567z" style="fill:#fff"/>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  <g>
+  </g>
+  </svg>
+`
 export interface SignupFormProps {
 
 }
@@ -159,6 +265,20 @@ class SignupForm extends Component<SignupFormProps, SignupFormState> {
                       required
                     />
 
+                    <div
+                      className="agreements password-view"
+                      dangerouslySetInnerHTML={{ __html: showPassword ? showsvg: hidesvg }}
+                      onClick={() => this.setState({ showPassword: !showPassword})}
+                    >
+                      {/* <input 
+                        type="checkbox"
+                        className="form-check-input"
+                        checked={showPassword}
+                        // value={ showPassword }
+                        onChange={(e) => this.setState({ showPassword: e.currentTarget.checked })}
+                      />
+                      <span className="form-check-label">Show Password</span> */}
+                    </div>
                   </div>
                   {
                     !passwordValidate &&
@@ -184,27 +304,20 @@ class SignupForm extends Component<SignupFormProps, SignupFormState> {
                       </small>
                     </div>
                   }
-                  <div className="form-row agreements">
-                    <input 
-                      type="checkbox"
-                      className="form-check-input"
-                      checked={showPassword}
-                      // value={ showPassword }
-                      onChange={(e) => this.setState({ showPassword: e.currentTarget.checked })}
-                    />
-                    <span className="form-check-label">Show Password</span>
-                  </div>
                   <div className="form-row agreements my-1">
-                    <input
-                      type="checkbox"
-                      className="form-check-input"
-                      // value={ this.state.privacyPolicy }
-                      checked={privacyPolicy}
-                      onChange={ (e) => this.setState({ privacyPolicy: e.currentTarget.checked }) }
-                    />
-                    <span className="form-check-label text-white">
-                      Get AWS tips and tricks delivered right to your inbox.
-                    </span>
+                    <div>
+                      {privacyPolicy   
+                        ? <img 
+                            src={BallotImage} width="20px" 
+                            onClick={() => this.setState({ privacyPolicy: !this.state.privacyPolicy})}
+                          />
+                        : <img 
+                            src={BallotImageUncheck} width="20px"
+                            onClick={() => this.setState({ privacyPolicy: !this.state.privacyPolicy})}
+                          />
+                      }
+                      <small className="text-white">Get AWS tips and tricks delivered right to your inbox.</small>
+                    </div>
                   </div>
                   <div className={`form-row my-4 ${!signupFailed && `mb-1`}`}>
                     <button type="submit" className="btn btn-primary mb-2 login-button">
